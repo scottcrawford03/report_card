c1afb7dfe29ced492ea9e91e22763e61684d05f0
Scott Crawford <scottcrawford03@gmail.com>
2014-11-07 14:56:34 -0700

    added average grades method

--- lib/grade_book.rb
+++ lib/grade_book.rb
@@ -1,3 +1,7 @@
+# require_relative '../lib/grade_book'   # => true
+require_relative '../lib/student'      # => true
+require_relative '../lib/report_card'  # => true
+
 class GradeBook
   attr_reader :grades         # => nil
   def initialize
@@ -7,29 +11,37 @@ class GradeBook
 
   def self.load_data
     [
-      { student_name: 'Alice Smith', subject: 'Biology', grade: 72 },
-      { student_name: 'Alice Smith', subject: 'Biology', grade: 89 },
-      { student_name: 'Alice Smith', subject: 'Creative Writing', grade: 92 },
-      { student_name: 'Bob Smith', subject: 'Creative Writing', grade: 94 },
-      { student_name: 'Cindy Johnson', subject: 'Video and Media Technology', grade: 86 }
-    ].map { |row| Student.new(row)  }
+      { student_name: 'Alice Smith', subject: 'Biology', grade: 72 },           # => {:student_name=>"Alice Smith", :subject=>"Biology", :grade=>72}
+      { student_name: 'Alice Smith', subject: 'Biology', grade: 89 },           # => {:student_name=>"Alice Smith", :subject=>"Biology", :grade=>89}
+      { student_name: 'Alice Smith', subject: 'Creative Writing', grade: 92 },  # => {:student_name=>"Alice Smith", :subject=>"Creative Writing", :grade=>92}
+      { student_name: 'Bob Smith', subject: 'Biology', grade: 94 },             # => {:student_name=>"Bob Smith", :subject=>"Biology", :grade=>94}
+      { student_name: 'Cindy Johnson', subject: 'Biology', grade: 86 }          # => {:student_name=>"Cindy Johnson", :subject=>"Biology", :grade=>86}
+    ].map { |row| Student.new(row)  }                                           # => [#<Student:0x007f8d4909a1e0 @student_name="Alice Smith", @subject="Biology", @grade=72>, #<Student:0x007f8d4909a1b8 @student_name="Alice Smith", @subject="Biology", @grade=89>, #<Student:0x007f8d4909a190 @student_name="Alice Smith", @subject="Creative Writing", @grade=92>, #<Student:0x007f8d4909a140 @student_name="Bob Smith", @subject="Biology", @grade=94>, #<Student:0x007f8d4909a118 @student_name="Cindy Johnson", @subject="Biology", @grade=86>]
   end
 
   def self.grades
-    @grades ||= self.load_data
+    @grades ||= self.load_data  # => [#<Student:0x007f8d4909a1e0 @student_name="Alice Smith", @subject="Biology", @grade=72>, #<Student:0x007f8d4909a1b8 @student_name="Alice Smith", @subject="Biology", @grade=89>, #<Student:0x007f8d4909a190 @student_name="Alice Smith", @subject="Creative Writing", @grade=92>, #<Student:0x007f8d4909a140 @student_name="Bob Smith", @subject="Biology", @grade=94>, #<Student:0x007f8d4909a118 @student_name="Cindy Johnson", @subject="Biology", @grade=86>]
   end
 
   def self.all_of_grades(student)
     grades.find_all { |row| row.student_name == student }
   end
 
-  def self.grades_by_class(subject, student=nil)
-    class_grades = grades.find_all { |row| row.subject == subject }
-    class_grades.find_all { |row| row.student_name == student } if student
+  def self.grades_by_class(subject)#, student=nil)
+    class_grades = grades.find_all { |row| row.subject == subject }  # => [#<Student:0x007f8d4909a1e0 @student_name="Alice Smith", @subject="Biology", @grade=72>, #<Student:0x007f8d4909a1b8 @student_name="Alice Smith", @subject="Biology", @grade=89>, #<Student:0x007f8d4909a140 @student_name="Bob Smith", @subject="Biology", @grade=94>, #<Student:0x007f8d4909a118 @student_name="Cindy Johnson", @subject="Biology", @grade=86>]
+    # class_grades.find_all { |row| row.student_name == student } if student  # => nil
   end
 
   def self.sort_grades(student)
     self.all_of_grades(student).sort { |a,b| b.grade <=> a.grade }
   end
 
+  def self.average_grades(subject_name)
+    subject = grades_by_class(subject_name)                                      # => [#<Student:0x007f8d4909a1e0 @student_name="Alice Smith", @subject="Biology", @grade=72>, #<Student:0x007f8d4909a1b8 @student_name="Alice Smith", @subject="Biology", @grade=89>, #<Student:0x007f8d4909a140 @student_name="Bob Smith", @subject="Biology", @grade=94>, #<Student:0x007f8d4909a118 @student_name="Cindy Johnson", @subject="Biology", @grade=86>]
+    average_grades = subject.reduce(0) {|sum, n| sum += n.grade }/ subject.size  # => 85
+    "#The average grade for #{subject_name} is #{average_grades}"                # => "#The average grade for Biology is 85"
+  end
+
 end
+
+  GradeBook.average_grades("Biology")  # => "#The average grade for Biology is 85"
--- lib/report_card.rb
+++ lib/report_card.rb
@@ -1,4 +1,5 @@
 class ReportCard
+  attr_reader :student
   def initialize(student)
     @student = student
   end
@@ -14,4 +15,11 @@ class ReportCard
     max_length = subject_grades.max_by { |row| row.student_name.length }.student_name.length + 3
     subject_grades.map { |row| "#{row.student_name.ljust(max_length, '.')}#{row.grade}"  }
   end
+
+  def self.average_grades(subject)
+    subject = grades_by_class(subject)
+    average_grades = subject.reduce(0) {|sum, n| sum += n.grade }/ subject.size
+    "#The average grade for #{subject} is {average_grades}"
+  end
+  
 end
